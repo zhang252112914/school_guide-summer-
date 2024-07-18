1ecc141073aba26ba30efee0a3823a4bd843bd2c
Skye-ye <saber.ap.saikou@gmail.com>
2024-07-18 21:44:51 +0800

    Fix the outofindex bug in GetEdgeSlot

--- campus_tour_guide_system/campus_map.cpp
+++ campus_tour_guide_system/campus_map.cpp
@@ -211,8 +211,8 @@ void CampusMap::SearchNodeSlot(double pos_x, double pos_y, Sender sender) {
 void CampusMap::GetEdgeSlot(Sender sender) {
   QVector<QPair<QPair<double, double>, QPair<double, double>>> coordinate_pairs;
   for (const auto& edge : edges) {
-    const Node& n1 = nodes[edge.end_node_one_id];
-    const Node& n2 = nodes[edge.end_node_two_id];
+    const Node& n1 = nodes[edge.end_node_one_id - 1];
+    const Node& n2 = nodes[edge.end_node_two_id - 1];
     QPair<double, double> coord1 = qMakePair(n1.pos_x, n1.pos_y);
     QPair<double, double> coord2 = qMakePair(n2.pos_x, n2.pos_y);
     coordinate_pairs.append(qMakePair(coord1, coord2));
